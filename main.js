da215560bfd7bdef7850db5211ef841955fbab59
Mae <funj451@gmail.com>
2023-05-01 19:21:40 +0700

    updated js file

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 import "./style.css";
+
+const currencyElement = document.getElementById("currency-one");
+const currencyElementTwo = document.getElementById("currency-two");
+const currencyAmountOne = document.getElementById("amount-one");
+const currencyAmountTwo = document.getElementById("amount-two");
+
+const rateElement = document.getElementById("rate");
+const swap = document.getElementById("swap");
+
+function calculate() {
+  const currencyOne = currencyElement.value;
+  const currencyTwo = currencyElementTwo.value;
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currencyOne}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const rate = data.rates[currencyTwo];
+      rateElement.innerText = `1 ${currencyOne} = ${rate} ${currencyTwo}`;
+      currencyAmountTwo.value = (currencyAmountOne.value * rate).toFixed(2);
+    });
+}
+
+currencyElement.addEventListener("change", calculate);
+currencyElementTwo.addEventListener("change", calculate);
+currencyAmountOne.addEventListener("input", calculate);
+currencyAmountTwo.addEventListener("input", calculate);
+
+calculate();
